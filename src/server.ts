45bc879a0ef82e0af4ce520f8c84baab4a5ee7e5
Guilherme Cordeiro <guilherme.helly@gmail.com>
2020-05-13 17:08:00 -0300

    Adição do body-parser

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
 import routes from './routes';
 
-const server = express();
+const app = express();
 
-server.use(routes);
+app.use(bodyParser.json());
+app.use(routes);
 
-server.listen(3333, () => {
+app.listen(3333, () => {
   console.log('Server started on port 3333');
 });
